97ee1ac9a34d6769913c189ded3a57f4489dbd97
andre <andrecabralpires@gmail.com>
2020-04-15 08:18:14 +0100

    mapbox lecture

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,9 +26,13 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
+import { initMapbox } from '../plugins/init_mapbox'
+import { initAutocomplete } from '../plugins/init_autocomplete'
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
+  initAutocomplete();
 });
---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,44 @@
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  const fitMapToMarkers = (map, markers) =>{
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+    map.fitBounds(bounds, { padding: 60, maxZoom: 20, duration: 0 });
+  }
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/apires89/cjpb0eog5a2tx2spj2qhqytwz'
+    });
+    map.addControl(new MapboxGeocoder( { accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }));
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); //pop up info
+
+        // Create a HTML element for your custom marker
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+      new mapboxgl.Marker(element)
+       .setLngLat([ marker.lng, marker.lat ])
+       .setPopup(popup)
+       .addTo(map);
+    });
+    fitMapToMarkers(map, markers);
+  }
+}
+
+export { initMapbox };
+
